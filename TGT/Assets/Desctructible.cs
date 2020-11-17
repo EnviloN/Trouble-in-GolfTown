aa65cf102f5157ad22079bbdb38ed5301afc8708
AnnaMoudra <moudrann@fit.cvut.cz>
2020-11-17 20:49:37 +0100

    Tower near Coast - destruction started, not yet working properly.

---
+++ TGT/Assets/Desctructible.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desctructible : MonoBehaviour
+{
+    public GameObject destroyedTower;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(destroyedTower, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }
+    }
+
+}
