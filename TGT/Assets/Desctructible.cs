06ed9001932e7e5d9dcbc77c8d38f300ba2110f5
AnnaMoudra <moudrann@fit.cvut.cz>
2020-11-21 13:20:25 +0100

    Scale out animation with smoke done for all towers.

--- TGT/Assets/Desctructible.cs
+++ TGT/Assets/Desctructible.cs
@@ -2,14 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Desctructible : MonoBehaviour
 {
     public GameObject destroyedTower;
+    public ParticleSystem smoke;
+
+    public float lifetime = 30.0f;
+    private int smoke_number = 7;
+
+    private GameObject tower;
+
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            Instantiate(destroyedTower, transform.position, transform.rotation);
+        if (Input.GetKeyDown(KeyCode.D)) {
+            tower = Instantiate(destroyedTower, transform.position, transform.rotation);
+
+            ParticleSystem[] clones = new ParticleSystem[smoke_number+1];
+            
+            for (var i = 0; i < smoke_number; i++) {
+                clones[i] = Instantiate(smoke, transform.position + Vector3.right * Random.Range(-8,8) + Vector3.down * Random.Range(-2, 10) + Vector3.back * Random.Range(-7, 7), smoke.transform.rotation);
+            }
+            clones[smoke_number] = Instantiate(smoke, transform.position + Vector3.down , smoke.transform.rotation);
+
+            
+            for (var i = 0; i <= smoke_number; i++)
+            {
+                Destroy(clones[i], lifetime + 15f);
+            }
+
             Destroy(this.gameObject);
         }
     }
---
+++ TGT/Assets/ScaleOut.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleOut : MonoBehaviour
+{
+
+    public float fadeSpeed = .05f;
+    public IEnumerator ScaleOutTower()
+    {
+
+        while (this.transform.localScale.y > 0.0)
+        {
+            Vector3 objectScale = this.transform.localScale;
+            float fadeAmount = (fadeSpeed * Time.deltaTime);
+
+            Debug.Log("localScale: " + objectScale + fadeAmount);
+            objectScale = new Vector3(objectScale.x - fadeAmount, objectScale.y - fadeAmount, objectScale.z - fadeAmount);
+            this.transform.localScale = objectScale;
+            yield return null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ScaleOutTower());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
