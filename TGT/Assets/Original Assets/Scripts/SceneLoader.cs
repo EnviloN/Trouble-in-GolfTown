a841271d32d29b524c2f5c62cf21653adf0e6a77
Ján Chudý <chudyja1@fit.cvut.cz>
2020-11-26 07:25:31 +0100

    Loading subscenes when world is loaded

---
+++ TGT/Assets/Original Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Awake() {
+        LoadMainScene();
+    }
+    private static void LoadSceneAdditively(string sceneName) {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+
+    private static void LoadMainScene() {
+        LoadSceneAdditively("Town");
+        LoadSceneAdditively("Dock");
+        LoadSceneAdditively("MagnatesResidence");
+        LoadSceneAdditively("TrainStation");
+        LoadSceneAdditively("Cemetery");
+        LoadSceneAdditively("MinigolfCourses");
+        LoadSceneAdditively("Towers");
+    }
+}
