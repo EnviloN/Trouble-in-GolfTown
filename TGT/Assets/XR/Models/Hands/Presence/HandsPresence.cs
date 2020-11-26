a36dd90e9bc56f71641a78ac9dd7975063067492
Dominik Fryč <frycdomi@fit.cvut.cz>
2020-11-27 00:55:59 +0100

    Hands Presence and animations

---
+++ TGT/Assets/XR/Models/Hands/Presence/HandsPresence.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandsPresence : MonoBehaviour
+{
+	public InputDeviceCharacteristics controllerCharacteristics;
+	public GameObject handModelPrefab;
+	private InputDevice targetDevice;
+	private GameObject spawnedHandModel;
+	private Animator handAnimator;
+
+    void Start()
+    {
+    	TryInitialize();
+    }
+
+    void TryInitialize() {
+    	List<InputDevice> devices = new List<UnityEngine.XR.InputDevice>();
+		InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
+		targetDevice = devices[0];
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+		handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+    	if (!targetDevice.isValid)
+    	{
+    		TryInitialize();
+    	}
+    	else
+    	{
+    		if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+			{
+				handAnimator.SetFloat("Trigger", triggerValue);
+			}
+			else
+			{
+				handAnimator.SetFloat("Trigger", 0);
+			}
+			if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+			{
+				handAnimator.SetFloat("Grip", gripValue);
+			}
+			else
+			{
+				handAnimator.SetFloat("Grip", 0);
+			}
+    	}
+    }
+}
