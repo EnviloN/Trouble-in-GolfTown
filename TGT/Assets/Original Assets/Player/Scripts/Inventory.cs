4176eb220e36ad3ac9e25f48d5b1afc9fbb3c10c
Honza Picka <j.picka97@gmail.com>
2020-12-12 12:49:34 +0100

    Spawn clubs in players hand on keypress, change near clipping pane of main camera to 0.01

--- TGT/Assets/Original Assets/Player/Scripts/Inventory.cs
+++ TGT/Assets/Original Assets/Player/Scripts/Inventory.cs
@@ -6,6 +6,19 @@ public class Inventory : MonoBehaviour {
 
     [SerializeField] private int numOfBallsVar;
 
+    public KeyCode switchClubKey;
+    public GameObject putterPrefab;
+    public GameObject fiveIronPrefab;
+    private int clubInHandState = 0;
+    private GameObject clubObject = null;
+    private float relativeClubDistance = 0.3f;
+    private Quaternion clubRotation = Quaternion.Euler(35f, 20f, 180f);
+
+    public KeyCode switchBallKey;
+    public float maxBallSpawnDistance = 3f;
+    public GameObject spawnableGolfBallPrefab;
+    private bool ballSpawningState = false;
+
     public int numOfBalls {
         get => numOfBallsVar;
     }
@@ -27,6 +40,51 @@ public class Inventory : MonoBehaviour {
     void Start() {
         resetInventory();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(switchClubKey))
+        {
+            switch(clubInHandState)
+            {
+                case 0: // Neither of clubs is in player hand
+                    // Instantiate Putter in player's hand
+                    clubObject = Instantiate(putterPrefab, transform.position + (transform.right * relativeClubDistance), transform.rotation * clubRotation);
+                    clubInHandState = 1;
+                    break;
+                case 1: // Putter in player's hand
+                    // Remove Putter from player's hand and instatiate 5Iron
+                    Destroy(clubObject);
+                    clubObject = Instantiate(fiveIronPrefab, transform.position + (transform.right * relativeClubDistance), transform.rotation * clubRotation);
+                    clubInHandState = 2;
+                    break;
+                case 2: // 5Iron in player's hand
+                    // Remove 5Iron from player's hand
+                    Destroy(clubObject);
+                    clubInHandState = 0;
+                    break;
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        PositionClubInPlayersHand();
+    }
+
+    #region ItemsInHand methods
+
+    private void PositionClubInPlayersHand()
+    {
+        if (clubObject != null)
+        {
+            clubObject.transform.position = transform.position + (transform.right * relativeClubDistance);
+            clubObject.transform.rotation = transform.rotation * clubRotation;
+
+        }
+    }
+
+    #endregion
 
     #region Inventory methods
 
